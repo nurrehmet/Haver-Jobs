7932d1733cf341f026cc08349599b2368d8a7f7c
Imam Nurrahmat <nurrehmet@email.unikom.ac.id>
2020-04-10 15:30:41 +0700

    Membuat Marker dari Database

--- lib/components/maps_view.dart
+++ lib/components/maps_view.dart
@@ -1,9 +1,12 @@
 import 'dart:async';
 import 'dart:ffi';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/services.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:solid_bottom_sheet/solid_bottom_sheet.dart';
 
 class MapsView extends StatefulWidget {
   @override
@@ -11,7 +14,9 @@ class MapsView extends StatefulWidget {
 }
 
 class MapsViewState extends State<MapsView> {
-  Position _currentPosition;
+  final Firestore _database = Firestore.instance;
+  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
+  SolidController _bsController = SolidController();
   double lat, long;
   CameraPosition _myLocation;
   Completer<GoogleMapController> _controller = Completer();
@@ -24,16 +29,17 @@ class MapsViewState extends State<MapsView> {
   @override
   void initState() {
     _getCurrentLocation();
+    createMarker();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       body: Stack(
         children: <Widget>[
           GoogleMap(
+            markers: Set<Marker>.of(markers.values),
             mapType: MapType.normal,
             initialCameraPosition:
                 _myLocation == null ? _defaultLocation : _myLocation,
@@ -41,47 +47,80 @@ class MapsViewState extends State<MapsView> {
               _controller.complete(controller);
             },
           ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Container(
-              height: 200,
-              width: MediaQuery.of(context).size.width,
-              child: Card(
-                semanticContainer: true,
-                clipBehavior: Clip.antiAliasWithSaveLayer,
-                child: Column(
-                  children: <Widget>[
-                    Spacer(),
-                    Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: FloatingActionButton.extended(
-                        onPressed: () => null,
-                        label: Text('Cari Karyawan'),
-                        icon: Icon(Icons.directions),
-                      ),
-                    ),
-                  ],
-                ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                elevation: 5,
-                margin: EdgeInsets.all(10),
+          Padding(padding: const EdgeInsets.all(16.0), child: Container()),
+          SafeArea(
+            child: Align(
+              alignment: Alignment.topRight,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: FloatingActionButton(
+                    child: Icon(Icons.location_searching),
+                    onPressed: () => _currentLocation()),
               ),
-            ),
-          ),
-          Align(
-            alignment: Alignment.bottomRight,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: FloatingActionButton(
-                  child: Icon(Icons.location_searching),
-                  onPressed: () => _currentLocation()),
             ),
           )
         ],
       ),
+      bottomSheet: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SolidBottomSheet(
+          controller: _bsController,
+          draggableBody: true,
+          headerBar: Container(
+            height: 50,
+            child: Icon(
+              Icons.maximize,
+              size: 50,
+              color: Colors.grey,
+            ),
+          ),
+          body: Container(
+            height: 30,
+            child: Center(
+              child: FloatingActionButton.extended(
+                onPressed: () {},
+                label: Text('Cari Karyawan'),
+                icon: Icon(Icons.person_pin),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
+  }
+
+ Future<void>  createMarker() async {
+    await _database
+        .collection("users")
+        .where("role", isEqualTo: "employee")
+        .getDocuments()
+        .then((docs) {
+      if (docs.documents.isNotEmpty) {
+        for (int i = 0; i < docs.documents.length; i++) {
+          initMarker(docs.documents[i].data, docs.documents[i].documentID);
+        }
+      }
+    }).catchError((e){
+      print(e);
+    },);
+  }
+
+  void initMarker(user, userid) {
+    var markerIdVal = userid;
+    final MarkerId markerId = MarkerId(markerIdVal);
+
+    // creating a new MARKER
+    final Marker marker = Marker(
+      markerId: markerId,
+      position: LatLng(user['latitude'], user['longitude']),
+      infoWindow: InfoWindow(title: user['nama'], snippet: user['noHp']),
+    );
+
+    setState(() {
+      // adding a new marker to map
+      markers[markerId] = marker;
+      print(markerId);
+    });
   }
 
   Future<void> _currentLocation() async {
@@ -93,7 +132,7 @@ class MapsViewState extends State<MapsView> {
     final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
 
     geolocator
-        .getCurrentPosition(desiredAccuracy: LocationAccuracy.best)
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high)
         .then((Position position) {
       setState(() {
         lat = position.latitude;
